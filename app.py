96274d710adeff0f527aacc15b0d5af351b87ff5
Jake Brucks <jakobpbrucks@gmail.com>
2020-10-06 11:07:52 -0500

    new viz pages

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ import psycopg2
 import pandas as pd
 # from bson import json_util
 import json
-import datetime
+# import datetime
 
 # import my password from config.py
-from config import password
+# from config import password
 
 # create engine and connection to postgres
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
-db_name = "avocado_db"
+# db_name = "avocado_db"
 # engine = create_engine(f'postgresql://postgres:{password}@localhost:5432/{db_name}')
-engine = create_engine('postgres://hqvlqzjiivxhbq:2939ea5341bc0ffdd6b426d3b5dce6b2530fffa968c8171db36b48fc24d71d0d@ec2-52-200-82-50.compute-1.amazonaws.com:5432/d14jp5o6rlp0b8')
-connection = engine.connect()
-connection
+# engine = create_engine('postgres://hqvlqzjiivxhbq:2939ea5341bc0ffdd6b426d3b5dce6b2530fffa968c8171db36b48fc24d71d0d@ec2-52-200-82-50.compute-1.amazonaws.com:5432/d14jp5o6rlp0b8')
+# connection = engine.connect()
+# connection
 
 app = Flask(__name__)
 
@@ -31,96 +31,96 @@ def index():
 
     return render_template("index.html", avo_data=avo_data, transport=transport) #weather=weather, bananas=bananas, gas=gas
 
-@app.route("/api/v1.0/data")
-def get_data():
-    avo_data = {}
+# @app.route("/api/v1.0/data")
+# def get_data():
+#     avo_data = {}
     
-    # AVOCADO PRICES DATA ---
-    # import SQL table as pandas dataframe
-    avocado_df = pd.read_sql('select * from avocado', connection)
+#     # AVOCADO PRICES DATA ---
+#     # import SQL table as pandas dataframe
+#     avocado_df = pd.read_sql('select * from avocado', connection)
     
-    # convert pandas dataframe to json
-    avocado_json = json.dumps(avocado_df.to_dict('records'), default=str)
+#     # convert pandas dataframe to json
+#     avocado_json = json.dumps(avocado_df.to_dict('records'), default=str)
     
-    avo_data['avocado_prices'] = avocado_json
-    # --- AVOCADO PRICES DATA ---
+#     avo_data['avocado_prices'] = avocado_json
+#     # --- AVOCADO PRICES DATA ---
 
-    # GAS PRICES DATA ---
-    # import SQL table as pandas dataframe
-    gas_df = pd.read_sql('select * from gas', connection)
+#     # GAS PRICES DATA ---
+#     # import SQL table as pandas dataframe
+#     gas_df = pd.read_sql('select * from gas', connection)
     
-    # convert pandas dataframe to json
-    gas_json = json.dumps(gas_df.to_dict('records'), default=str)
+#     # convert pandas dataframe to json
+#     gas_json = json.dumps(gas_df.to_dict('records'), default=str)
     
-    avo_data['gas_prices'] = gas_json
-    # --- GAS PRICES DATA ---
+#     avo_data['gas_prices'] = gas_json
+#     # --- GAS PRICES DATA ---
 
-    # TOT TRANSPORT PRICES DATA ---
-    # import SQL table as pandas dataframe
-    tot_tr_df = pd.read_sql('select * from tot_transport', connection)
+#     # TOT TRANSPORT PRICES DATA ---
+#     # import SQL table as pandas dataframe
+#     tot_tr_df = pd.read_sql('select * from tot_transport', connection)
     
-    # convert pandas dataframe to json
-    tot_tr_json = json.dumps(tot_tr_df.to_dict('records'), default=str)
+#     # convert pandas dataframe to json
+#     tot_tr_json = json.dumps(tot_tr_df.to_dict('records'), default=str)
     
-    avo_data['tot_transport'] = tot_tr_json
-    # --- TOT TRANSPORT PRICES DATA ---
+#     avo_data['tot_transport'] = tot_tr_json
+#     # --- TOT TRANSPORT PRICES DATA ---
 
-    #  AVO TRANSPORT PRICES DATA ---
-    # import SQL table as pandas dataframe
-    avo_tr_df = pd.read_sql('select * from avo_transport', connection)
+#     #  AVO TRANSPORT PRICES DATA ---
+#     # import SQL table as pandas dataframe
+#     avo_tr_df = pd.read_sql('select * from avo_transport', connection)
     
-    # convert pandas dataframe to json
-    avo_tr_json = json.dumps(avo_tr_df.to_dict('records'), default=str)
+#     # convert pandas dataframe to json
+#     avo_tr_json = json.dumps(avo_tr_df.to_dict('records'), default=str)
     
-    avo_data['avo_transport'] = avo_tr_json
-    # --- AVO TRANSPORT PRICES DATA ---
+#     avo_data['avo_transport'] = avo_tr_json
+#     # --- AVO TRANSPORT PRICES DATA ---
 
-    #  WEATHER DATA ---
-    # import SQL table as pandas dataframe
-    weather_df = pd.read_sql('select * from san_diego', connection)
-    weather_df2 = pd.read_sql('select * from san_diego2', connection)
+#     #  WEATHER DATA ---
+#     # import SQL table as pandas dataframe
+#     weather_df = pd.read_sql('select * from san_diego', connection)
+#     weather_df2 = pd.read_sql('select * from san_diego2', connection)
 
-    # convert pandas dataframe to json
-    weather_json = json.dumps(weather_df.to_dict('records'), default=str)
-    weather_json2 = json.dumps(weather_df2.to_dict('records'), default=str)
+#     # convert pandas dataframe to json
+#     weather_json = json.dumps(weather_df.to_dict('records'), default=str)
+#     weather_json2 = json.dumps(weather_df2.to_dict('records'), default=str)
 
-    avo_data['weather'] = weather_json
-    avo_data['weather2'] = weather_json2
-    # --- WEATHER DATA ---
+#     avo_data['weather'] = weather_json
+#     avo_data['weather2'] = weather_json2
+#     # --- WEATHER DATA ---
 
-    #  BANANA PRICES DATA ---
-    # import SQL table as pandas dataframe
-    bananas_df = pd.read_sql('select * from banana_prices', connection)
+#     #  BANANA PRICES DATA ---
+#     # import SQL table as pandas dataframe
+#     bananas_df = pd.read_sql('select * from banana_prices', connection)
     
-    # convert pandas dataframe to json
-    bananas_json = json.dumps(bananas_df.to_dict('records'), default=str)
+#     # convert pandas dataframe to json
+#     bananas_json = json.dumps(bananas_df.to_dict('records'), default=str)
     
-    avo_data['bananas'] = bananas_json
-    # --- BANANA PRICES DATA ---
+#     avo_data['bananas'] = bananas_json
+#     # --- BANANA PRICES DATA ---
 
-    return avo_data
+#     return avo_data
 
-@app.route("/api/v1.0/transport")
-def get_transport():
+# @app.route("/api/v1.0/transport")
+# def get_transport():
 
-    transport = {}
-    # TRANSPORT & PRICES DATA ---
-    # import SQL table as pandas dataframe
-    tot_tr_df = pd.read_sql('select * from tot_transport', connection)
-    avo_tr_df = pd.read_sql('select * from avo_transport', connection)
-    avocado_df = pd.read_sql('select * from avocado', connection)
+#     transport = {}
+#     # TRANSPORT & PRICES DATA ---
+#     # import SQL table as pandas dataframe
+#     tot_tr_df = pd.read_sql('select * from tot_transport', connection)
+#     avo_tr_df = pd.read_sql('select * from avo_transport', connection)
+#     avocado_df = pd.read_sql('select * from avocado', connection)
 
-    # convert pandas dataframe to json
-    tot_tr_json = json.dumps(tot_tr_df.to_dict('records'), default=str)
-    # avo_tr_json = json.dumps(avo_tr_df.to_dict('records'), default=str)
-    # avocado_json = json.dumps(avocado_df.to_dict('records'), default=str)
+#     # convert pandas dataframe to json
+#     tot_tr_json = json.dumps(tot_tr_df.to_dict('records'), default=str)
+#     # avo_tr_json = json.dumps(avo_tr_df.to_dict('records'), default=str)
+#     # avocado_json = json.dumps(avocado_df.to_dict('records'), default=str)
     
-    # transport["tot"] = tot_tr_json
-    # transport["avo"] = avo_tr_json
-    # transport["price"] = avocado_json
+#     # transport["tot"] = tot_tr_json
+#     # transport["avo"] = avo_tr_json
+#     # transport["price"] = avocado_json
     
 
-    return tot_tr_json
+#     return tot_tr_json
 
     # cursor = connection.execute('SELECT * FROM tot_transport;')
     # tot_tr_df = cursor.fetchall() 
--- static/js/logic.js
+++ static/js/logic.js
@@ -29,37 +29,43 @@ $(document).ready(function(){
 // -- FUNCTION TO HIDE TITLE BLOCK --
 
   // initialize charts for rewriting with input data
-function init() {
+// function init() {
     
-    // Set up chart
-    var trace = {
-      x: [],
-      y: [],
-      mode: 'markers',
-      marker: {
-        size: []
-      }
-    };
+//     // Set up chart
+//     var trace = {
+//       x: [],
+//       y: [],
+//       mode: 'markers',
+//       marker: {
+//         size: []
+//       }
+//     };
     
-    var data = [trace];
+//     var data = [trace];
     
-    var layout = {
-      title: 'Charts',
-      showlegend: false,
-    };
+//     var layout = {
+//       title: 'Charts',
+//       showlegend: false,
+//     };
     
-    var chart = d3.select("#chart").node();
+//     var chart = d3.select("#chart").node();
     
-    Plotly.newPlot(chart, data, layout);
-}
+//     Plotly.newPlot(chart, data, layout);
+// }
 
 // -- TRANSPORT DATA FUNCTIONS AND INFO --
 d3.select(".avocado__skin").on("click", transport);
-
   function transport() {
+
+    console.log('open page')
+    window.open(
+      'ml_plots_jpb.html',
+      // '_blank' // <- This is what makes it open in a new window.
+      );
+    };
     // console.log("it worked");
-    d3.json("/api/v1.0/transport").then((data)=>{
-      console.log(JSON.parse(JSON.stringify(data)))
+    // d3.json("/api/v1.0/transport").then((data)=>{
+      // console.log(JSON.parse(JSON.stringify(data)))
       // var tot_transport = data.tot
       // var avo_transport = data.avo
       // var avo_prices = data.price
@@ -116,160 +122,166 @@ d3.select(".avocado__skin").on("click", transport);
       // dashboard_title.text("USDA Transport Data");
       // dashboard_subTitle.text("Trucking Availability and Rates");
       // dashboard_text.text("This data comes from the USDA Ag Transport site https://agtransport.usda.gov/, specifically the trucking category. This data shows the availability and rates for refrigerated trucks transporting ag commodities and we utilized this and were able to drill down to see costs associated specifically with avocados.");
-    });      
-};
+    // });      
+// };
 // -- TRANSPORT DATA FUNCTIONS AND INFO --
 
 // -- WEATHER DATA FUNCTIONS AND INFO --
 d3.select(".avocado__inner").on("click", weather);
-
+  
   function weather() {
+    console.log('open page')
+    location.replace("pages/avoWeather.html",
+      '_parent'
+      );
+    };
+
     // console.log("it worked");
-    d3.json("/api/v1.0/data").then((data)=>{
+    // d3.json("/api/v1.0/data").then((data)=>{
       // replotting weather visualizations
       
-      var weatherTrace = {
-        x: [],
-        y: [],
-        mode: 'markers',
-        marker: {
-          size: []
-        }
-      };
+      // var weatherTrace = {
+      //   x: [],
+      //   y: [],
+      //   mode: 'markers',
+      //   marker: {
+      //     size: []
+      //   }
+      // };
       
-      var weatherData = [weatherTrace];
+      // var weatherData = [weatherTrace];
     
-      var weatherLayout = {
-        title: 'Weather Data Analysis',
-        showlegend: false,
-      };
+      // var weatherLayout = {
+      //   title: 'Weather Data Analysis',
+      //   showlegend: false,
+      // };
     
-      var chart = d3.select("#chart").node();
+      // var chart = d3.select("#chart").node();
     
-      Plotly.react(chart, weatherData, weatherLayout);
+      // Plotly.react(chart, weatherData, weatherLayout);
     
       // grab nodes for updating info card
-      var dashboard_titleW = d3.select("#dashboard_title");
-      var dashboard_subTitleW = d3.select("#dashboard_subtitle");
-      var dashboard_textW = d3.select("#dashboard_text");
+      // var dashboard_titleW = d3.select("#dashboard_title");
+      // var dashboard_subTitleW = d3.select("#dashboard_subtitle");
+      // var dashboard_textW = d3.select("#dashboard_text");
     
       // Update info card with new text
-      dashboard_titleW.text("Open Weather API");
-      dashboard_subTitleW.text("San Diego Historical Weather");
-      dashboard_textW.text("Info on this data, limitations, etc.");
-    }); 
-  };
+  //     dashboard_titleW.text("Open Weather API");
+  //     dashboard_subTitleW.text("San Diego Historical Weather");
+  //     dashboard_textW.text("Info on this data, limitations, etc.");
+  //   }); 
+  // };
 // -- WEATHER DATA FUNCTIONS AND INFO --
 
 // -- GAS DATA FUNCTIONS AND INFO --
-d3.select(".avocado__inner-shadow").on("click", gas);
+// d3.select(".avocado__inner-shadow").on("click", gas);
 
-  function gas() {
+  // function gas() {
     // console.log("it worked");
-    d3.json("/api/v1.0/data").then((data)=>{
+    // d3.json("/api/v1.0/data").then((data)=>{
     // replotting gas visualizations
     
-      var gasTrace = {
-        x: [],
-        y: [],
-        mode: 'markers',
-        marker: {
-          size: []
-        }
-      };
+      // var gasTrace = {
+      //   x: [],
+      //   y: [],
+      //   mode: 'markers',
+      //   marker: {
+      //     size: []
+      //   }
+      // };
     
-      var gasData = [gasTrace];
+      // var gasData = [gasTrace];
     
-      var gasLayout = {
-        title: 'Gas Data Analysis',
-        showlegend: false,
-      };
+      // var gasLayout = {
+      //   title: 'Gas Data Analysis',
+      //   showlegend: false,
+      // };
     
-      var chart = d3.select("#chart").node();
+      // var chart = d3.select("#chart").node();
     
-      Plotly.react(chart, gasData, gasLayout);
+      // Plotly.react(chart, gasData, gasLayout);
     
       // grab nodes for updating info card
-      var dashboard_titleG = d3.select("#dashboard_title");
-      var dashboard_subTitleG = d3.select("#dashboard_subtitle");
-      var dashboard_textG = d3.select("#dashboard_text");
+      // var dashboard_titleG = d3.select("#dashboard_title");
+      // var dashboard_subTitleG = d3.select("#dashboard_subtitle");
+      // var dashboard_textG = d3.select("#dashboard_text");
     
       // Update info card with new text
-      dashboard_titleG.text("US Energy Information Administration Data");
-      dashboard_subTitleG.text("Gas Prices Data");
-      dashboard_textG.text("Info on this data, limitations, etc.");
-    });
-  };
+  //     dashboard_titleG.text("US Energy Information Administration Data");
+  //     dashboard_subTitleG.text("Gas Prices Data");
+  //     dashboard_textG.text("Info on this data, limitations, etc.");
+  //   });
+  // };
 // -- GAS DATA FUNCTIONS AND INFO --
 
 // -- BANANA DATA FUNCTIONS AND INFO --
-d3.select(".avocado__seed").on("click", banana);
+// d3.select(".avocado__seed").on("click", banana);
 
-  function banana() {
+  // function banana() {
     // console.log("it worked");
-    d3.json("/api/v1.0/data").then((data)=>{
+    // d3.json("/api/v1.0/data").then((data)=>{
     // replotting banana visualizations
-      var avocado_prices = data.avocado_prices
-      var banana_prices = data.bananas
-      var gas_prices = data.gas_prices
+//       var avocado_prices = data.avocado_prices
+//       var banana_prices = data.bananas
+//       var gas_prices = data.gas_prices
       
-      var bananaTrace = {
-        x: banana_prices[0].date,
-        y: banana_prices[0].price_per_pound,
-        mode: 'lines',
-        type: 'scatter',
-        name: "Bananas",
-        line: {
-          color: 'yellow'
-        }
-      };
-      var gasTrace = {
-        x: gas_prices[0].date,
-        y: gas_prices[0].gas_all_grades,
-        mode: 'lines',
-        type: 'scatter',
-        name: 'Gas',
-        line: {
-          color: 'black'
-        }
-      };
-      var avocadoTrace = {
-        x: avocado_prices[0].date,
-        y: avocado_prices[0].average_price,
-        mode: 'lines',
-        type: 'scatter',
-        name: 'Avocado',
-        line: {
-          color: 'green'
-        }
-      };
-      var compareData = [avocadoTrace, bananaTrace, gasTrace];
-      var compareLayout = {
-        title: 'Overall Price Analysis',
-        showlegend: false,
-        xaxis: {
-          range: ['01/01/2015', '03/31/2018'],
-          type: 'date'
-        },
-        yaxis: {
-          range: [0, 5],
-          type: 'linear'
-        }
-      };
-      var chart = d3.select("#chart").node();
-      Plotly.react(chart, compareData, compareLayout);
+//       var bananaTrace = {
+//         x: banana_prices[0].date,
+//         y: banana_prices[0].price_per_pound,
+//         mode: 'lines',
+//         type: 'scatter',
+//         name: "Bananas",
+//         line: {
+//           color: 'yellow'
+//         }
+//       };
+//       var gasTrace = {
+//         x: gas_prices[0].date,
+//         y: gas_prices[0].gas_all_grades,
+//         mode: 'lines',
+//         type: 'scatter',
+//         name: 'Gas',
+//         line: {
+//           color: 'black'
+//         }
+//       };
+//       var avocadoTrace = {
+//         x: avocado_prices[0].date,
+//         y: avocado_prices[0].average_price,
+//         mode: 'lines',
+//         type: 'scatter',
+//         name: 'Avocado',
+//         line: {
+//           color: 'green'
+//         }
+//       };
+//       var compareData = [avocadoTrace, bananaTrace, gasTrace];
+//       var compareLayout = {
+//         title: 'Overall Price Analysis',
+//         showlegend: false,
+//         xaxis: {
+//           range: ['01/01/2015', '03/31/2018'],
+//           type: 'date'
+//         },
+//         yaxis: {
+//           range: [0, 5],
+//           type: 'linear'
+//         }
+//       };
+//       var chart = d3.select("#chart").node();
+//       Plotly.react(chart, compareData, compareLayout);
     
-      // grab nodes for updating info card
-      var dashboard_titleB = d3.select("#dashboard_title");
-      var dashboard_subTitleB = d3.select("#dashboard_subtitle");
-      var dashboard_textB = d3.select("#dashboard_text");
+//       // grab nodes for updating info card
+//       var dashboard_titleB = d3.select("#dashboard_title");
+//       var dashboard_subTitleB = d3.select("#dashboard_subtitle");
+//       var dashboard_textB = d3.select("#dashboard_text");
     
-      // Update info card with new text
-      dashboard_titleB.text("USDA Economic Research Service Data");
-      dashboard_subTitleB.text("Banana Prices Data");
-      dashboard_textB.text("Info on this data, limitations, etc.");
-    });
-};
-// -- BANANA DATA FUNCTIONS AND INFO --
+//       // Update info card with new text
+//       dashboard_titleB.text("USDA Economic Research Service Data");
+//       dashboard_subTitleB.text("Banana Prices Data");
+//       dashboard_textB.text("Info on this data, limitations, etc.");
+//     });
+// };
+// // -- BANANA DATA FUNCTIONS AND INFO --
 
-init();
+// init();
